8edc4eed227d363bfb76bbdaa8707f319dc8e85c
Jimmy Li <jimmyli921@gmail.com>
2018-01-28 19:32:21 -0800

    add chat routes, controller and model

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'chats/:username', to: 'chat#index'
+  get 'chat/:id', to: 'chat#show'
+  post 'chat', to: 'chat#create'
 end
