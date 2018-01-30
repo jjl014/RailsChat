a5dd57b7bf30d634596fec0560107c5eeef1542a
Jimmy Li <jimmyli921@gmail.com>
2018-01-29 23:21:43 -0800

    remove numericality validation from model

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -12,8 +12,7 @@
 #
 
 class Chat < ApplicationRecord
-  validates :username, :text, presence: true
-  validates :timeout, numericality: true
+  validates :username, :text,   presence: true
 
   def self.cached_chat(id)
     Rails.cache.fetch([self, "chat-#{id}"], expires_in: 30.minutes.to_i) {
